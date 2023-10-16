b559257dfc9b4a79542ee5b2a444f65df012f070
vedavyas <vedavyas14042003@gmail.com>
2023-10-16 19:49:14 +0530

    Added the first popup

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,9 @@ import Navbar from './components/Navbar.vue';
 
 <template>
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div> -->
     <Navbar/>
   </header>
+  
   <footer>
 
   </footer>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,7 +1,52 @@
 <script setup>
-
+import { ref } from 'vue'
+const popUp = ref(true)
+const removePopUp = () => {
+  popUp.value = false
+};
 </script>
 
 <template>
-<p>hello</p>
+  <div v-if="popUp" class="popUp">
+    <p>Website openning, lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+    <div class="remove">
+      <img src="@/assets/cross.svg" alt="crosssvg.." @click="removePopUp" class="crossBtn" />
+    </div>
+  </div>
 </template>
+
+<style>
+.popUp {
+  width: 100%;
+  height: 70px;
+  background: #ff5703;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  padding-block: 3rem;
+}
+.popUp > p{
+    text-align: center;
+}
+.remove {
+  position: absolute;
+  width: 17px;
+  height: 17px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  right: 2.5%;
+  top: 5px;
+}
+.remove:hover{
+    cursor: pointer;
+}
+.crossBtn{
+    width: 10px;
+    height: 12px;
+}
+</style>
