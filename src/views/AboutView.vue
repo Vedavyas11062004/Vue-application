de6d771be802d670eb287519282ba5f7dea1cdaf
vedavyas <vedavyas14042003@gmail.com>
2023-10-18 22:13:54 +0530

    About page created

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,8 +10,140 @@
       </p>
     </div>
     <div class="image_container">
-      <img src="@/assets/Picture.svg" alt="img2.."/>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <img src="@/assets/Picture.svg" alt="img2.." />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <div class="author">
+        <div class="author_img">
+          <!-- <img src="" alt=""/> -->
+        </div>
+        <div class="author_name">
+          <h2>John Doe</h2>
+          <p>See all posts</p>
+        </div>
+      </div>
+      <div class="line_div">
+        <img src="@/assets/Line.svg" class="line" />
+      </div>
+    </div>
+    <div class="news_cards">
+      <span class="title">TITLE SECTION LIGHT</span>
+      <div class="newscard_container">
+        <div class="top_part">
+          <img src="@/assets/Picture.svg" alt="img.." />
+        </div>
+        <div class="bottom_part">
+          <div class="category"><span>CATEGORY</span> par <span>Lorem Ipsum</span></div>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+        </div>
+      </div>
+      <div class="newscard_container">
+        <div class="top_part">
+          <img src="@/assets/Picture.svg" alt="img.." />
+        </div>
+        <div class="bottom_part">
+          <div class="category"><span>CATEGORY</span> par <span>Lorem Ipsum</span></div>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+        </div>
+      </div>
+      <div class="newscard_container">
+        <div class="top_part">
+          <img src="@/assets/Picture.svg" alt="img.." />
+        </div>
+        <div class="bottom_part">
+          <div class="category"><span>CATEGORY</span> par <span>Lorem Ipsum</span></div>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+        </div>
+      </div>
+      <div class="newscard_container">
+        <div class="top_part">
+          <img src="@/assets/Picture.svg" alt="img.." />
+        </div>
+        <div class="bottom_part">
+          <div class="category"><span>CATEGORY</span> par <span>Lorem Ipsum</span></div>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+        </div>
+      </div>
+      <div class="newscard_container">
+        <div class="top_part">
+          <img src="@/assets/Picture.svg" alt="img.." />
+        </div>
+        <div class="bottom_part">
+          <div class="category"><span>CATEGORY</span> par <span>Lorem Ipsum</span></div>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+        </div>
+      </div>
+      <div class="line_div">
+        <img src="@/assets/Line.svg" class="line" />
+      </div>
+    </div>
+    <div class="news_cards">
+      <span class="title">TITLE SECTION LIGHT</span>
+      <div class="newscard_container">
+        <div class="top_part">
+          <img src="@/assets/Picture.svg" alt="img.." />
+        </div>
+        <div class="bottom_part">
+          <div class="category"><span>CATEGORY</span> par <span>Lorem Ipsum</span></div>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+        </div>
+      </div>
+      <div class="newscard_container">
+        <div class="top_part">
+          <img src="@/assets/Picture.svg" alt="img.." />
+        </div>
+        <div class="bottom_part">
+          <div class="category"><span>CATEGORY</span> par <span>Lorem Ipsum</span></div>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+        </div>
+      </div>
+      <div class="newscard_container">
+        <div class="top_part">
+          <img src="@/assets/Picture.svg" alt="img.." />
+        </div>
+        <div class="bottom_part">
+          <div class="category"><span>CATEGORY</span> par <span>Lorem Ipsum</span></div>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+        </div>
+      </div>
+      <div class="newscard_container">
+        <div class="top_part">
+          <img src="@/assets/Picture.svg" alt="img.." />
+        </div>
+        <div class="bottom_part">
+          <div class="category"><span>CATEGORY</span> par <span>Lorem Ipsum</span></div>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+        </div>
+      </div>
+      <div class="newscard_container">
+        <div class="top_part">
+          <img src="@/assets/Picture.svg" alt="img.." />
+        </div>
+        <div class="bottom_part">
+          <div class="category"><span>CATEGORY</span> par <span>Lorem Ipsum</span></div>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -41,7 +173,7 @@
   font-weight: 700;
 }
 
-.image_container{
+.image_container {
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -53,9 +185,85 @@
   width: 90%;
   margin-inline: auto;
 }
-.image_container > p{
+.image_container > p {
   width: 90%;
   font-size: 1rem;
   font-family: 'Libre Baskerville', serif;
+}
+
+.author {
+  display: flex;
+  gap: 1rem;
+  align-self: flex-start;
+  width: 90%;
+  margin-inline: auto;
+}
+.author_img {
+  width: 60px;
+  height: 60px;
+  background: #d9d9d9;
+}
+.author_name h2 {
+  font-size: 1.25rem;
+  font-weight: 700;
+}
+
+.title {
+  display: block;
+  width: 90%;
+  margin-inline: auto;
+  margin-bottom: 0.5rem;
+  font-size: 1.25rem;
+  font-weight: 600;
+}
+
+.news_cards {
+  margin-block: 1rem;
+}
+
+.newscard_container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.top_part {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-block: 1.5rem;
+}
+.top_part > img {
+  width: 90%;
+  margin-inline: auto;
+}
+.bottom_part {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 90%;
+  margin-inline: auto;
+  gap: 0.5rem;
+}
+.bottom_part > h2 {
+  text-align: center;
+  font-weight: 700;
+  font-size: 1.5rem;
+  line-height: 27px;
+}
+.bottom_part > p {
+  text-align: center;
+  font-family: 'Libre Baskerville', serif;
+}
+.category span {
+  font-weight: 700;
+}
+
+.line_div {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-block: 2rem;
 }
 </style>
