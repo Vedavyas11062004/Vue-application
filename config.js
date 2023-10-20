989b4776567f35159c4267f9b27aaab7fdb90ad6
vedavyas <vedavyas14042003@gmail.com>
2023-10-20 10:45:28 +0530

    Added config file

---
+++ config.js
@@ -0,0 +1,15 @@
+
+export default {
+  font1: 'League Spartan',
+  font2: 'Libre Baskerville',
+  color1: '#003C57',
+  color2: '#FF5703',
+  menuIDs: ['menuID1', 'menuID2', 'menuID3'],
+  socialMediaLinks: 'social media links',
+  categoryShortID: 'categoryshort ID',
+  categoryIDs: ['category1ID', 'category2ID', 'category3ID'],
+  link1: {
+    valueConnected: 'Value connected',
+    valueNotConnected: 'Value not connected'
+  }
+}
